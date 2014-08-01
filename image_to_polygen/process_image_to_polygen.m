6c0ba7ebb8c7963dd492cf6a8bb57dbcd52cfa51
Yitong <yitong@Yitongs-MacBook-Pro.local>
2014-08-01 15:39:01 +0800

    to stdout

--- image_to_polygen/process_image_to_polygen.m
+++ image_to_polygen/process_image_to_polygen.m
@@ -34,10 +34,8 @@ function process_image_to_polygen(img_name)
     
     [x,y] = find(border == 1);
     %fid = fopen('points', 'w');
-    %fprintf(fid, '%d %d\n', [y,x]');
+    fprintf(1, '%d %d\n', [y,x]');
     %fclose(fid);
-
-    [y,x]
     
     %subplot(1,3,1);
     %figure(2);
