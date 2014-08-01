0ecba5aa8d79feda79fb51cc9e24a82d0762d498
Hao Hong <agate.hao@gmail.com>
2014-08-01 16:40:21 +0800

    improved demo

--- image_dumper/run.sh
+++ image_dumper/run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-ruby map_html_builder.rb $@ > map.html
-phantomjs snapshot.js
+DIR="../results/$1+$2"
+mkdir -p $DIR
+
+ruby map_html_builder.rb $@ > $DIR/map.html
+
+http_proxy=www-proxy.corp.factual.com:3128
+
+if [ -z "$http_proxy" ]; then
+  phantomjs snapshot.js $DIR
+else
+  phantomjs --proxy=$http_proxy snapshot.js $DIR
+fi  
