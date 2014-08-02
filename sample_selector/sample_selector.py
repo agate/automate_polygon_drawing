390e01b09246df6fe67a890517dff2788887b416
dongshu <dongshu@factual.com>
2014-08-02 21:22:55 +0800

    sampling the points by recursive splitting

--- sample_selector/sample_selector.py
+++ sample_selector/sample_selector.py
@@ -1,10 +1,11 @@
 #!/usr/local/bin/python
 import numpy as np
+import math
 import sys
 from PIL import Image
 
 def get_polygon_from_file(input_file):
-    global polygon
+    polygon = {}
     filename = open(input_file, 'r')
     points = []
     for line in filename:
@@ -12,103 +13,118 @@ def get_polygon_from_file(input_file):
     filename.close()
     return polygon
 
-def get_start_point(polygon):
-    return sorted(polygon.keys(), key=lambda tup: tup[0])[0]
+def get_init_points(polygon):
+    sorted_tuples = sorted(polygon.keys(), key=lambda tup: tup[0])
+    start = sorted_tuples[0]
+    end = sorted_tuples[-1]
+    return [start, end]
 
-def draw_polygon(points, width, height):
-    data = np.zeros( (width, height, 3), dtype=np.uint8)
-    for x in range(width):
-        for y in range(height):
+def draw_polygon(points, output_file):
+    data = np.zeros( (800, 800, 3), dtype=np.uint8)
+    for x in range(800):
+        for y in range(800):
             if (x, y) in points:
                 data[x, y] = [255, 0, 0]
     img = Image.fromarray(data, 'RGB')
-    img.save('sample.png')
+    img.save(output_file)
 
-def checkDirection(direction):
-    global p_x
-    global p_y
+def checkDirection(direction, border):
+    global delta
     global polygon
-    if direction == 1:
-        if (p_x-1, p_y-1) in polygon:
-            p_x = p_x - 1
-            p_y = p_y - 1
-            return True
-    if direction == 2:
-        if (p_x, p_y-1) in polygon:
-            p_x = p_x
-            p_y = p_y - 1
-            return True
-    if direction == 3:
-        if (p_x+1, p_y-1) in polygon:
-            p_x = p_x + 1
-            p_y = p_y - 1
-            return True
-    if direction == 4:
-        if (p_x+1, p_y) in polygon:
-            p_x = p_x + 1
-            p_y = p_y
-            return True
-    if direction == 5:
-        if (p_x+1, p_y+1) in polygon:
-            p_x = p_x + 1
-            p_y = p_y + 1
-            return True
-    if direction == 6:
-        if (p_x, p_y+1) in polygon:
-            p_x = p_x
-            p_y = p_y + 1
-            return True
-    if direction == 7:
-        if (p_x-1, p_y+1) in polygon:
-            p_x = p_x - 1
-            p_y = p_y + 1
-            return True
-    if direction == 8:
-        if (p_x-1, p_y) in polygon:
-            p_x = p_x - 1
-            p_y = p_y
-            return True
+    p = border[-1]
+    (deltax, deltay) = delta[direction]
+    if (p[0] + deltax, p[1] + deltay) in polygon:
+        border.append((p[0] + deltax, p[1] + deltay))
+        return True
     return False
 
-def move_to_next(direction):
-    direction = (direction + 3 + 1) % 8 + 1
-    for x in range(direction, 9):
-        if checkDirection(x):
-            return x
-    for x in range(1, direction):
-        if checkDirection(x):
-            return x
-
-def traversal_image(interval):
+def move_to_next(border):
     global direction
-    global p_x
-    global p_y
-    step = 0
-    points = []
-    start_x = p_x
-    start_y = p_y
+    direction = (direction + 4 + 1) % 8
+    for x in range(direction, direction+7):
+        if checkDirection(x%8, border):
+            direction = x
+            return
+
+def get_border(p1):
+    global direction
+    border = []
+    border.append(p1)
     while True:
-        print p_x, p_y
-        direction = move_to_next(direction)
-        # sample by slope
-#        if next_direction != direction:
-#            points.append((p_x, p_y))
-        # sample every interval points
-        step = step + 1
-        if step % interval == 0:
-            points.append((p_x, p_y))
-        if start_x == p_x and start_y == p_y:
+        move_to_next(border)
+        if (border[-1] == p1):
             break
+    return border
+
+def compute_distance(p1, p2):
+    return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
+
+def compute_square(p0, p1, p2, d):
+    d01 = math.sqrt(compute_distance(p1, p0))
+    d02 = math.sqrt(compute_distance(p2, p0))
+    d12 = math.sqrt(d)
+    p = (d01 + d02 + d12)/2
+    return abs(p*(p-d01)*(p-d02)*(p-d12))
+
+def find_the_farest_point(start, end):
+    global threshold
+    global border
+    max_h = 0
+    index = start + 1
+    d = compute_distance(border[start], border[end])
+    for x in range(start+1, end):
+        height = compute_square(border[x], border[start], border[end], d) * 4 / d
+        if max_h < height:
+            max_h = height
+            index = x
+    if threshold < max_h:
+        return index
+    return start
+
+def recursive_splitting(start, middle, end):
+    global sample
+    index = find_the_farest_point(start, middle)
+    if (index != start):
+        sample.append(index)
+        recursive_splitting(start, index, middle)
+
+    index = find_the_farest_point(middle, end)
+    if (index != middle):
+        sample.append(index)
+        recursive_splitting(middle, index, end)
+
+def get_points_from_index(sample, border):
+    points = []
+    for index in sample:
+        points.append(border[index])
     return points
 
-polygon = {}
-p_x = 0
-p_y = 0
-direction = 5
 if __name__ == "__main__":
+    delta = {0:(-1,-1), 1:(0,-1), 2:(1,-1), 3:(1,0), 4:(1,1), 5:(0,1), 6:(-1,1), 7:(-1,0)}
+
     input_file = sys.argv[1]
-    interval = int(sys.argv[2])
-    get_polygon_from_file(input_file)
-    (p_x, p_y) = get_start_point(polygon)
-    points = traversal_image(interval)
-    print '\n'.join('%d %d' % p for p in points)
+    polygon = get_polygon_from_file(input_file)
+    [p1, p2] = get_init_points(polygon)
+
+    direction = 5
+    border = get_border(p1)
+
+    sample = []
+    start = 0
+    middle = border.index(p2)
+    end = len(border)-1
+    sample.append(start)
+    sample.append(middle)
+    sample.append(end)
+
+    threshold = int(sys.argv[2]) ** 2
+    recursive_splitting(start, middle, end)
+
+    sample = sorted(sample)
+    points = get_points_from_index(sample, border)
+    for point in points:
+        print point
+
+    #draw_polygon(polygon, "polygon.png")
+    #draw_polygon(border, "border.png")
+    #draw_polygon(points, "sample.png")
